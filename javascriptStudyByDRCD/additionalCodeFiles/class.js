49f26d0e0f98581326c51497fb7dd0d10cc5a41f
jeahun10717 <jeahun10717@gmail.com>
2020-09-24 11:50:19 +0900

    2020/09/24 1차 upload

--- javascriptStudyByDRCD/additionalCodeFiles/class.js
+++ javascriptStudyByDRCD/additionalCodeFiles/class.js
@@ -1,8 +1,39 @@
-class Experiment {
-    publicField = 2;
-    #privateField = 0;
+class Shape {
+    constructor(width, height, color){
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw(){
+        console.log(`drawing ${this.color} color of`);
+    }
+    getArea(){
+        return this.width * this.height
+    }
 }
 
-const experiment = new Experiment();
-console.log(experiment.publicField);
-console.log(experiment.privateField);
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+    draw(){
+        console.log('△');
+    }
+    getArea(){
+        return(this.width * this.height) / 2;
+    }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
+console.log(rectangle.getArea());
+
+const triangle = new Triangle(20, 20, 'red');
+triangle.draw();
+console.log(triangle.getArea());
+
+console.log('-----------check of instance----------');
+console.log(rectangle instanceof Rectangle);
+console.log(triangle instanceof Rectangle);
+console.log(triangle instanceof Triangle);
+console.log(triangle instanceof Shape);
+console.log(triangle instanceof Object);
